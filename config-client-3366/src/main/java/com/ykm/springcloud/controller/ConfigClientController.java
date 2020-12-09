512475008e42ee1241fc7c5a2275ff54584db5af
yangkemin <yangkemin@jikugroup.com>
2020-12-09 20:28:31 +0800

    springcloud-bus

--- config-client-3366/src/main/java/com/ykm/springcloud/controller/ConfigClientController.java
+++ config-client-3366/src/main/java/com/ykm/springcloud/controller/ConfigClientController.java
@@ -14,12 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RefreshScope
 public class ConfigClientController {
 
+    @Value("${server.port}")
+    private String serverPort;
+
     @Value("${config.info}")
     private String configInfo;
 
     @GetMapping("/configInfo")
     public String getConfigInfo() {
-        return configInfo;
+        return "configInfo:" + configInfo + ",serverPort:" + serverPort;
     }
 
 }
