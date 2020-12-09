df47452118728bc62aa338c551abf875f914966e
yangkemin <yangkemin@jikugroup.com>
2020-12-09 12:00:55 +0800

    gateway-断言/过滤器

---
+++ gateway-gateway9527/src/test/java/T2.java
@@ -0,0 +1,12 @@
+import java.time.ZonedDateTime;
+
+/**
+ * @author ykm
+ * @date 2020/12/9 11:26 上午
+ */
+public class T2 {
+    public static void main(String[] args) {
+        ZonedDateTime zdt = ZonedDateTime.now();
+        System.out.println(zdt);
+    }
+}
